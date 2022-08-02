e651148db9095eb2db0758577df3e22bc0676e3b
maurifabris <maurifabris91@gmail.com>
2022-08-01 21:48:58 -0300

    commit pokemon

--- proyecto1.js
+++ proyecto1.js
@@ -1,57 +1,41 @@
-/*
-let nike = 50
-
-let puma = 40
-let adidas = 55
-let sandalias = 30
-let precio = 0
-let confirm = si
-
-function agregar(marca){
-  do{
-    marca = prompt("que marca desea agregar?").toLocaleLowerCase
-    confirm = prompt("desea agregar mas productos?").toLocaleLowerCase
-    return precio + marca
-  } while( confirm === si)
-}
-agregar(puma)
-console.log(precio)*/
-////////////////////////////
-////////////////////////
-
-
-let velocidad = parseFloat(prompt("velocidad?"))
-
-function aumentarVelocidad(vel){
-  while(vel < 100){ 
-  vel += 10
-  console.log(vel)
-}}
-function reducirVelocidad(vel){
-  while(vel > 100){ 
-  vel -= 10
-  console.log(vel)
-}}
-
-if( velocidad > 100 ) {
-  reducirVelocidad(velocidad)
-} else{
-  aumentarVelocidad(velocidad)
+class Pokemon{
+  constructor(nombreParam, claseParam, vidaParam){
+    this.nombre = nombreParam
+    this.clase = claseParam
+    this.vida = vidaParam
+  }
+  atacar(){
+    console.log(`${this.nombre} ataco causando ${this.ataque} de daño`)
+  }
 }
 
+class Ataque{
+  constructor(NombreAtc, danioAtc){
+    this.Nombre = NombreAtc
+    this.daño = danioAtc
+  }
+}
+
+const Charizard1 = new Pokemon("Charizard", "Fuego", 250)
+const Charmeleon2  = new Pokemon("Charizard", "Fuego", 270)
+const Bulbasaur3= new Pokemon("Bulbasaur", "Planta", 300)
+const Ivysaur4 = new Pokemon("Ivysaur ", "Planta", 320)
+const Squirtle5 = new Pokemon("Squirtle", "Agua", 350)
+const Wartortle6  = new Pokemon("Wartortle ", "Planta",370)
+
+const Latigazo = new Ataque("Latigo sepa", 50)
+const lanzaLlamas = new Ataque("Lanza llamas", 70)
+const burbujas = new Ataque("Burbujas", 45)
+
+
+const ataques = [Latigazo, lanzaLlamas, burbujas]
+let pokemonesPlanta = [Bulbasaur3, Ivysaur4]
+let PokemonesAgua = [Squirtle5, Wartortle6]
+let PokemonesFuego = [Charizard1, Charmeleon2]
+
+const TodosLosPokemon = pokemonesPlanta.concat(PokemonesAgua).concat(PokemonesFuego)
+
+console.log(TodosLosPokemon)
 
 
 
-
-
-
-
-////////////////////////////////////////////////
-/*
-let velocidad = 50
-let variacion
-do {
-  variacion = prompt(`Ingrese una opcion:  1-subri velocidad  2-bajar velocidad 3-mantener`)
-  switch
-
-} while(velocidad=50)*/
