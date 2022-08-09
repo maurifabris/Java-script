1c4a526ec12b884dfc013ba1a0ac5e40b8bf5afd
maurifabris <maurifabris91@gmail.com>
2022-08-09 00:03:29 -0300

    commit eventos

--- classPokemon.js
+++ classPokemon.js
@@ -1,78 +1,98 @@
-class Pokemon{
-    constructor(nombreParam, claseParam, vidaParam){
-      this.nombre = nombreParam
-      this.clase = claseParam
-      this.vida = vidaParam
-    }
-
-    //este metodo es para que reciban los ataques, tambien quiero hacer uno de usar pociones
-    recibirAtaque(danio){ 
-      if(this.vida - danio <= 0){
-        console.log(`${this.nombre} se devilito`)
-      } else {
-        console.log(`${this.nombre} aguanto el ataque y ahora tiene ${this.vida}`)
-    }
-        this.vida -= danio
-        }
+/*class Pokemon {
+  constructor(nombreParam, claseParam, vidaParam) {
+    this.nombre = nombreParam
+    this.clase = claseParam
+    this.vida = vidaParam
   }
- 
-class Charizard  extends Pokemon {
-    constructor(nombreParam, claseParam, vidaParam, danioLanzallamas){
-        super(nombreParam, claseParam, vidaParam)
-        this.danioLanzallamas = danioLanzallamas
-    }
 
-    //mas adelante la idea es que el usuario pueda elegir a quien atacar, lo podria hacer con un promt?
-    lanzallamas(PokemonAtacado){ 
-      if(PokemonAtacado.clase == "Agua"){
-          const parraLanzallamasAgua = document.getElementById("dialogojs")
-          parraLanzallamasAgua.innerText = (`${this.nombre} uso ¡Lanza llamas! Causando ${(this.danioLanzallamas * 0.85)} porque su rival es de tipo agua`)
-          PokemonAtacado.recibirAtaque(this.danioLanzallamas * 0.90)
+  //este metodo es para que reciban los ataques, tambien quiero hacer uno de usar pociones
+  recibirAtaque(danio) {
+    if (this.vida - danio <= 0) {
+      console.log(`${this.nombre} se devilito`)
+    } else {
+      console.log(`${this.nombre} aguanto el ataque y ahora tiene ${this.vida}`)
+    }
+    this.vida -= danio
+  }
+}
+
+class Charizard extends Pokemon {
+  constructor(nombreParam, claseParam, vidaParam, danioLanzallamas) {
+    super(nombreParam, claseParam, vidaParam)
+    this.danioLanzallamas = danioLanzallamas
+  }
+
+  //mas adelante la idea es que el usuario pueda elegir a quien atacar, lo podria hacer con un promt?
+  lanzallamas(PokemonAtacado) {
+    if (PokemonAtacado.clase == "Agua") {
+      const parraLanzallamasAgua = document.getElementById("dialogojs")
+      TodosLosPokemon.forEach(ataque =>){
+      divdialogojs
+
       }
-      if(PokemonAtacado.clase === "Planta"){
+
+
+
+      PokemonAtacado.recibirAtaque(this.danioLanzallamas * 0.90)
+    }
+    if (PokemonAtacado.clase === "Planta") {
       console.log(`${this.nombre} uso ¡Lanza llamas! Causando ${this.danioLanzallamas * 1.25} porque su rival es de tipo planta`)
-      PokemonAtacado.recibirAtaque(this.danioLanzallamas * 1.25)}
-      if(PokemonAtacado.clase === "Fuego"){
+      PokemonAtacado.recibirAtaque(this.danioLanzallamas * 1.25)
+    }
+    if (PokemonAtacado.clase === "Fuego") {
       console.log(`${this.nombre} uso ¡Lanza llamas! Causando ${this.danioLanzallamas}`)
       PokemonAtacado.recibirAtaque(this.danioLanzallamas)
-      } 
     }
-  }
-
-  
-
-  class Bulbasaur  extends Pokemon {
-    constructor(nombreParam, claseParam, vidaParam, danioLatigazo){
-        super(nombreParam, claseParam, vidaParam)
-        this.danio = danioLatigazo
-    }
-    latigazo(PokemonAtacado){
-      console.log(`${this.nombre} uso ¡Latigazo! causando ${this.danioLatigazo}`)
-      PokemonAtacado.recibirAtaque(this.danioLatigazo)
-    }
-  }
-
-
-  class Squirtle extends Pokemon {
-    constructor(nombreParam, claseParam, vidaParam, danioBurbujas){
-        super(nombreParam, claseParam, vidaParam)
-        this.danioBurbujas = danioBurbujas
-    }
-    burbujas(PokemonAtacado){
-      if(PokemonAtacado.clase === "Planta"){
-        const parraLanzallamasAgua = document.getElementById("dialogo2")
-        parraLanzallamasAgua.innerText = (`${this.nombre} uso ¡Burbujas! Causando ${(this.danioBurbujas * 0.85)} porque su rival es de tipo Planta`)
-        PokemonAtacado.recibirAtaque(this.danioBurbujas * 0.90)
-    }
-    if(PokemonAtacado.clase === "Fuego"){
-      const parraLanzallamasAgua = document.getElementById("dialogo2")
-      parraLanzallamasAgua.innerText = `${this.nombre} uso ¡Burbujas! Causando ${(this.danioBurbujas * 1.25)} porque su rival es de tipo Fuego`
-    PokemonAtacado.recibirAtaque(this.danioBurbujas * 1.25)}
-    if(PokemonAtacado.clase === "Agua"){
-    console.log(`${this.nombre} uso ¡Burbujas! Causando ${this.danioBurbujas}`)
-    PokemonAtacado.recibirAtaque(this.danioBurbujas)
-    } 
   }
 }
 
 
+
+class Bulbasaur extends Pokemon {
+  constructor(nombreParam, claseParam, vidaParam, danioLatigazo) {
+    super(nombreParam, claseParam, vidaParam)
+    this.danio = danioLatigazo
+  }
+  latigazo(PokemonAtacado) {
+    console.log(`${this.nombre} uso ¡Latigazo! causando ${this.danioLatigazo}`)
+    PokemonAtacado.recibirAtaque(this.danioLatigazo)
+  }
+}
+
+
+class Squirtle extends Pokemon {
+  constructor(nombreParam, claseParam, vidaParam, danioBurbujas) {
+    super(nombreParam, claseParam, vidaParam)
+    this.danioBurbujas = danioBurbujas
+  }
+  burbujas(PokemonAtacado) {
+    if (PokemonAtacado.clase === "Planta") {
+      const parraLanzallamasAgua = document.getElementById("dialogo2")
+      parraLanzallamasAgua.innerText = (`${this.nombre} uso ¡Burbujas! Causando ${(this.danioBurbujas * 0.85)} porque su rival es de tipo Planta`)
+      PokemonAtacado.recibirAtaque(this.danioBurbujas * 0.90)
+    }
+    if (PokemonAtacado.clase === "Fuego") {
+      const parraLanzallamasAgua = document.getElementById("dialogo2")
+      parraLanzallamasAgua.innerText = `${this.nombre} uso ¡Burbujas! Causando ${(this.danioBurbujas * 1.25)} porque su rival es de tipo Fuego`
+      PokemonAtacado.recibirAtaque(this.danioBurbujas * 1.25)
+    }
+    if (PokemonAtacado.clase === "Agua") {
+      console.log(`${this.nombre} uso ¡Burbujas! Causando ${this.danioBurbujas}`)
+      PokemonAtacado.recibirAtaque(this.danioBurbujas)
+    }
+  }
+}*/class producto { 
+  constructor(nombreParam, colorparam, precioparam) {
+    this.nombre = nombreParam
+    this.color = colorparam
+    this.precio = precioparam
+  } 
+  comprar(){
+      dinero -= this.precio
+      console.log(dinero)
+  }
+}
+
+const Motog30 = new producto ("moto g30", "negro", 15)
+const Samsungs20 = new producto ("samsung s20", "blanco", 20)
+const iPhone12 = new producto ("I Phone", "negro", 25)
--- objetosPokemon.js
+++ objetosPokemon.js
@@ -1,4 +1,4 @@
-const Charizard1 = new Charizard("charizard1","Fuego", 250,50)
+/*const Charizard1 = new Charizard("charizard1","Fuego", 250,50)
 const Charmeleon2  = new Charizard("Charmeleon", "Fuego", 270, 50)
 const Bulbasaur3= new Bulbasaur("Bulbasaur", "Planta", 300, 40)
 const Ivysaur4 = new Bulbasaur("Ivysaur ", "Planta", 320, 40)
@@ -17,5 +17,30 @@ do{
     }
     Squirtle5.burbujas(Charizard1)
 } while ( (Charizard1.vida > 0 ) || (Squirtle5.vida > 0) )
+*/
+const botonCompra1 = document.getElementById("botonCompra1")
+const botonCompra2 = document.getElementById("botonCompra2")
+const botonCompra3 = document.getElementById("botonCompra3")
+const compra = document.getElementById("compra")
+const buscador = document.getElementById("buscador")
+const dinero = 50
 
+botonCompra1.addEventListener("click", () => {
+    Motog30.comprar
+    console.log("comprado")
+})
+botonCompra2.addEventListener("click", () => {
+   iPhone12.comprar
+   console.log("comprado")
+   console.log(dinero)
+})
+botonCompra3.addEventListener("click", () => {
+    iPhone12.comprar
+    console.log("s")
+})
+
+
+buscador.addEventListener("change",()=>{
+   console.log(buscador.value)
+})
 
